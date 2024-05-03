ee3795221d496c0ec7f31d5d678a5f4c028cff66
Matheus F. Rodrigues <78821990+Blococraft@users.noreply.github.com>
2024-05-03 08:22:58 -0300

    Pré-Projeto 2024 C. Perrone
    1ª Parte do pré-projeto

---
+++ Cópia de CONFIG.PHP
@@ -0,0 +1,16 @@
+<?PHP 
+ $dbHost = 'localhost';
+ $dbUsername = 'root';
+ $dbPassword = '';
+ $dbName = 'FormLFeMaCh';
+
+ $conexao = new mysqli($dbHost,$dbUsername,$Password,$Name);
+
+ if(conexao->connect_errno){
+
+    echo "Erro"
+ }
+
+
+
+>
